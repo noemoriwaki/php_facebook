55340f54d46fa9adb5b0e469b5eb0ab48fb01172
森脇野枝 <moriwakinoe@moriwakinoenoMacBook-Pro.local>
2020-05-04 06:37:15 +0900

    ファイルを更新

--- facebook.php
+++ facebook.php
@@ -48,73 +48,13 @@ $result = selectAll($dbh);
 <!DOCTYPE html>
 <html lang="ja">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-        <meta name="description" content="PHPによるfacebookのクローンの作成練習のために作られたサイトです。">
-    <!-- OGPを設定する   -->
-        <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/ fb# prefix属性: http://ogp.me/ns/ prefix属性#">
- 
-        <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/ fb# prefix属性: http://ogp.me/ns/ prefix属性#">
-            <!-- ページのURL -->
-        <meta property="og:url" content=" ページの URL" />
-            <!-- ページの種類 -->
-        <meta property="og:type" content="facebook" />
-            <!-- ページのタイトル -->
-        <meta property="og:title" content="facebookのクローン" />
-            <!-- ページのディスクリプション -->
-        <meta property="og:description" content="PHPによるfacebookのクローンの作成練習のために作られたサイトです。" />
-            <!-- ページのサイト名 -->
-        <meta property="og:site_name" content="Facebook" />
-            <!-- サムネイル画像のURL -->
-        <meta property="og:image" content=" サムネイル画像の URL" />    
-    <!-- OGPここまで -->
         <link rel="stylesheet" href="css/facebook.css">
-        <link rel="stylesheet" href="css/header_footer.css">
-        <link rel="stylesheet" href="css/responsive.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
          <title>facebook</title>
     </head>
     <body>
-        <header class="header_top">
-            <div class="header_logo">
-                <img src="facebook_image/facebook.png" alt="facebookのアイコン画像">
-            </div>
-                <form  class="header_seach" action="result.php" method="GET">
-                <input type="text" name="search" placeholder="検索">
-                <button type="submit" name="name" value=""><i class="fas fa-search"></i></button>
-                </form>
-            <div class="header_nav">
-                <nav>
-                    <a href="facebook.php">プロフィール</a><!--自分のプロフィールホームへ飛ぶようにする（このページ）-->
-                    <a href="#">ホーム</a>
-                    <a href="friends.php">友達</a>
-                    <a href="#">お知らせ</a>
-                    <a href="login.php">ログイン</a>
-                </nav>
-            </div>
-        </header>
-        <div id="wrapper">
-            <div class="wrapper_image">
-                <!-- メインの画像 -->
-                <img src="facebook_image/IMG_0009.jpeg" alt="兄弟の写真">
-            </div>
-            <div id="users">
-                <a href="#"><p>Noe Moriwaki</p></a>
-                    <div class="users_img wrapper_img">
-                        <img src="facebook_image/icon.01.jpeg" alt="icon画像">
-                    </div>
-            </div>
-            <div class="wrapper_nav">
-                <nav>
-                    <a href="facebook.php">タイムライン</a>
-                    <a href="base.php">基本データ</a>
-                    <a href="friends.php">友達</a>
-                    <a href="photo.php">写真</a>
-                    <a href="#">アーカイブ</a>
-                    <a href="#">その他</a>
-                </nav>
-            </div>
-        </div>
+        <!-- 共通のヘッダー部分の読み込み -->
+        <?php include("header.php"); ?>
+
         <main>
             <div id="friends">
             <a  class="friends_link" href="#">友達</a><br>
--- result.php
+++ result.php
@@ -1,3 +1,15 @@
+<!-- ここからログインの為のコード -->
+<?php
+// phpの一番初めに記載する必要がある
+session_start();
+// ログインしていない場合はログイン画面へ飛ぶ　
+// if (!$_SETTION["login"]) {
+//     header('Location: login.php');
+//     exit;
+// }
+// $user = $_SETTION['user'];
+?>
+
 <?php
 // データーベースの読み込み
 require_once("database.php");
@@ -5,10 +17,19 @@ require_once("database.php");
 // var_dump($search);
 // ５、６のsearchは別物
 function selectAll($dbh, $search){
+    // LIKEは文字列が一致するもの「％」を前後に使うと、その文字列を含むという意味になる
     $stmt = $dbh->prepare('SELECT * FROM posts WHERE  post_message LIKE ?');
     $stmt->execute(["%{$search}%"]);
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+function users($dbh, $user_name) {
+    $stmt = $dbh->prepare('SELECT * FROM userData WHERE user_name');
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+}
+
+var_dump($_POST['user_name']);
 
 $result = selectAll($dbh, $_GET["search"]);
 // var_dump($result);
@@ -22,17 +43,24 @@ $result = selectAll($dbh, $_GET["search"]);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/result.css">
     <title>検索結果
     </title>
 </head>
 <body>
-    <?php foreach($result as $row):?>
-    <p><?php echo $row["post_message"];?></p>
-    <?php if(!empty($row['post_image'])):?>
- <!-- アップロードされた画像の表示 -->
-    <p><img class="upload_img" src="./upload_dir/<?php echo $row['post_image'];?>"></p>
-     <?php endif ?><br>
-     <?php endforeach ?>
+    <!-- 共通のヘッダー部分の読み込み -->
+    <?php include("header.php"); ?>
+
+    <!-- ここから検索結果の読み込み -->
+    <div id="post">
+        <?php foreach($result as $row):?>
+            <p><?php echo $row["post_message"];?></p>
+                <?php if(!empty($row['post_image'])):?>
+                 <!-- アップロードされた画像の表示 -->
+                 <p><img class="upload_img" src="./upload_dir/<?php echo $row['post_image'];?>"></p>
+                <?php endif ?><br>
+        <?php endforeach ?>
+    </div>
 
     
 </body>
