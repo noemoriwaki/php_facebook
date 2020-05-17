527ecd34f4ef95fd46570a58137c4a928f7c161c
森脇野枝 <moriwakinoe@moriwakinoenoMacBook-Pro.local>
2020-05-17 16:49:28 +0900

    いいね機能の修正

--- count.php
+++ count.php
@@ -1,31 +1,46 @@
-<?php
-
-// session_start();
-// if(empty($_SESSION['count'])) {
-//     $_SESSION['count'] = 0;
-// }
-// $_SESSION['count']++;
-
-// echo json_encode([
-//     'fav_id' => $_POST['fav_id'],
-//     'fav_count' => $_SESSION['count']]
-// );
-
-
-
-?>
 
 <?php
-session_start();
+// 関数は並べる
+// データベースに登録 投稿するときに必要　今は必要ではない
+// function create($dbh, $iine)
 
-$postId = $_POST['post_id'];
-
-if (empty($_SESSION['count'][$postId])) {
-  $_SESSION['count'] = array_merge($_SESSION['count'], [$postId => 0]);
+require_once("database.php");
+//↑これで$dbhができた
+function selectOne($dbh,$post_id){
+  $stmt = $dbh->prepare("SELECT * FROM posts WHERE post_id = ? ");//?にpost_idがはいる
+  $data = [];
+  $data[] = $post_id;//配列に入れていく
+  $stmt->execute($data);
+  //execute配列にする
+  return $stmt->fetch(PDO::FETCH_ASSOC); 
 }
-$_SESSION['count'][$postId]++;
+// 更新アップデート文
+// postテーブルにあるいいねを更新
+function update($dbh, $iine, $postId) {
+  $sql = 'UPDATE posts SET iine = ? WHERE post_id = ?';
+  $stmt = $dbh->prepare($sql);
+  $data = [];//変数はまず初期化する
+  $data[] = $iine;
+  $data[] = $postId;
+  return $stmt->execute($data);
+}
 
+
+// カウントに関する内容
+$postId = $_POST['post_id'];
+// ここでpost_idにはいる
+
+// 全ての投稿データを$resultに入れている
+$result = selectOne($dbh,$postId);
+
+// いいねを計算
+$iine = $result["iine"] + 1;
+
+// アップデート
+update($dbh, $iine, $postId);
+
+// 更新した数を返す
 echo json_encode([
   'post_id' => $_POST['post_id'], 
-  'fav_count' => $_SESSION['count'][$postId]
-]);
+  'fav_count' => $iine//いいねの数
+]);//配列になっている
